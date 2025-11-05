82553f997165ff77e0bf4723802ad18ae76410d1
Sarada Gayathri <231fa04262@gmail.com>
2025-11-05 08:41:27 +0530

    Temporary save

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,29 @@
+// import axios from 'axios'
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// })
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default api
+
+
 import axios from 'axios'
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: 'https://healthandfitness-2.onrender.com/api',
 
+=======
+  baseURL: import.meta.env.VITE_API_URL + '/api',
+>>>>>>> d51fce7 (Fix vite permission issue)
 })
 
 api.interceptors.request.use((config) => {
@@ -14,5 +35,4 @@ api.interceptors.request.use((config) => {
 })
 
 export default api
-
 
